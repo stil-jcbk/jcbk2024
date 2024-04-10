508bb422fe60d6dee9efa011fc9fa3671e0b18dd
stil-jcbk <kubkon134@gmail.com>
2024-04-10 20:07:52 +0200

    projects section

---
+++ src/sections/projects/projects.tsx
@@ -0,0 +1,15 @@
+import "./style.scss"
+import Project from "../../components/project/project"
+
+export default function ProjectsSection() {
+  return(
+    <div className="projects">
+      <h2 className="headline">PROJECTS</h2>
+      <div className="projects-wrapper">
+      <Project name='PROJECT NAME' img='https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg' tools={["React, Typescript, NodeJS, Express, Firebase"]} />
+      <Project name='PROJECT NAME' img='https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg' tools={["React, Typescript, NodeJS, Express, Firebase"]} />
+      <Project name='PROJECT NAME' img='https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg' tools={["React, Typescript, NodeJS, Express, Firebase"]} />
+      </div>
+    </div>
+  )
+}
