c0b448a8d3b83d04f73e08cc59dd323ae592a28c
stil-jcbk <kubkon134@gmail.com>
2024-04-10 20:09:39 +0200

    changed tag div to section

--- src/sections/projects/projects.tsx
+++ src/sections/projects/projects.tsx
@@ -3,13 +3,13 @@ import Project from "../../components/project/project"
 
 export default function ProjectsSection() {
   return(
-    <div className="projects">
+    <section className="projects">
       <h2 className="headline">PROJECTS</h2>
       <div className="projects-wrapper">
       <Project name='PROJECT NAME' img='https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg' tools={["React, Typescript, NodeJS, Express, Firebase"]} />
       <Project name='PROJECT NAME' img='https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg' tools={["React, Typescript, NodeJS, Express, Firebase"]} />
       <Project name='PROJECT NAME' img='https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg' tools={["React, Typescript, NodeJS, Express, Firebase"]} />
       </div>
-    </div>
+    </section>
   )
 }
