96a4ab70d4485134638521c9c3f5fd7337957ffd
stil-jcbk <kubkon134@gmail.com>
2024-04-11 09:04:22 +0200

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './App.scss';
+import Navbar from './components/navbar/navbar';
+import HeroSection from './sections/hero/hero';
+import AboutMeSection from './sections/aboutme/aboutme';
+import ProjectsSection from './sections/projects/projects';
+import ContactSection from './sections/contact/contact';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
-    <></>
+    <>
+      <Navbar/>
+      <div className="website-content">
+        <HeroSection/>
+        <AboutMeSection/>
+        <ProjectsSection/>
+        <ContactSection/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
