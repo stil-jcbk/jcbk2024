b53b1ae4bde5c99c28fbb0bfe5aaee3813a5a5a8
stil-jcbk <kubkon134@gmail.com>
2024-04-11 09:05:34 +0200

    added href for socials

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,13 +5,13 @@ export default function Navbar(){
     <nav className="navbar">
       <span className="logo">JCBK</span>
       <div className="redirections">
-        <a href="/#">
+        <a href="https://github.com/stil-jcbk" target="_blank" rel="noreferrer">
           <img className="icon" src="/assets/github.svg" alt="github" />
         </a>
-        <a href="/#">
+        <a href="https://linkedin.com/in/jcbkdev" target="_blank" rel="noreferrer">
           <img className="icon" src="/assets/linkedin.svg" alt="linkedin" />
         </a>
-        <a href="/#">
+        <a href="mailto:jcbkdev@gmail.com" target="_blank" rel="noreferrer">
           <img className="icon" src="/assets/mail.svg" alt="mail" />
         </a>
       </div>
